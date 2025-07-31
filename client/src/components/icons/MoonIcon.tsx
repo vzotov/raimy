4a18173679c8844ac8ca114420e4155345a13774
Vladimir Zotov <vladimir@irreverentlabs.com>
2025-07-31 12:34:24 -0400

    Introduced themes Created ThemeSelector.tsx Updated initial look of the app

---
+++ client/src/components/icons/MoonIcon.tsx
@@ -0,0 +1,22 @@
+interface IconProps {
+  className?: string;
+}
+
+export default function MoonIcon({ className = "h-5 w-5" }: IconProps) {
+  return (
+    <svg
+      className={className}
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+      />
+    </svg>
+  );
+}
---
+++ client/src/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as MoonIcon } from './MoonIcon';
+export { default as SunIcon } from './SunIcon';
+export { default as CogIcon } from './CogIcon';
